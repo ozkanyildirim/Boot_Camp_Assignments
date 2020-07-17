3f6f6bdb52db02577a888a9e85ca450bdaad3696
ozkanyildirim <65204189+ozkanyildirim@users.noreply.github.com>
2020-07-17 18:02:26 +0200

    Update prime_numbers_function.py
    Added a test line for inapplicable entries.

--- prime_numbers_function.py
+++ prime_numbers_function.py
@@ -3,9 +3,11 @@ def primer(b = 1):
     Returns all integer values within two numbers. When it is not stated gives values from 1.
     """
     try:
-        n = int(input("Enter a value (n) to display prime numbers between 1 and 'n': "))
+        n = int(input(f"Enter a value (n) to display prime numbers between {b} and 'n': "))
         prime_list = []
-        if b == 1:
+        if b >= n:
+            print(f"Inapplicable demand. Defined range is {b} - {n}.")
+        elif b == 1:
             for i in range(2, n):
                 for j in range (2, i + 1):
                     if i % j == 0 and i != 2:
@@ -19,6 +21,7 @@ def primer(b = 1):
                         break
                     elif i == j + 1 and i % j != 0 or i == 2:
                         prime_list.append(i)
+         
         return prime_list
     except:
         print("Please enter a valid integer number")
