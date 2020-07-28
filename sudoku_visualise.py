862ea59fecb12922f468b8eefb18b3d056000efe
ozkanyildirim <ozkanyildirim.de@gmail.com>
2020-07-28 02:36:20 +0200

    Assignment:SUDOKU displaying ---> flexible and reusable

--- sudoku_visualise.py
+++ sudoku_visualise.py
@@ -1,19 +1,18 @@
-sudoku = [
-    [0, 0, 0, 0, 6, 4, 0, 0, 0],
-    [7, 0, 0, 0, 0, 0, 3, 9, 0],
-    [8, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 5, 0, 2, 0, 6, 0],
-    [0, 8, 0, 4, 0, 0, 0, 0, 0],
-    [3, 5, 0, 6, 0, 0, 0, 7, 0],
-    [0, 0, 2, 0, 0, 0, 1, 0, 3],
-    [0, 0, 1, 0, 5, 9, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 7, 0, 0]
-]
-
-count = 0 
-for i in sudoku:
-    if count % 3 == 0:
-        print(*'-'*11)
-    print(*i[0:3], '|', *i[3:6], '|', *i[6:9])
-    count += 1
-print(*'-'*11)
+def sudoku_display_unit(sudoku):
+    '''
+    Reshapes a given array inorer to get a predefined display format of SUDOKU. 
+    No matter what the input size output will be in the predefined format. 
+    
+    '''
+    count = 0 
+    for i in sudoku:
+        if count % 3 == 0:
+            print(*'-'*(len(i) + int((len(i) / 3)-1)))
+        
+        for j in range(len(i)):
+                if j == (len(i)-3):
+                    print(*i[-3:])
+                elif j % 3 == 0:
+                    print(*i[j:j + 3], '|', end=' ')
+        count += 1
+    print(*'-'*(len(i) + int((len(i) / 3)-1)))
